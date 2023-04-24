235e9a069e0cd0b31c29b95442afe543b7362005
Fachrur Rozi Syah Putra Berutu <rozymc99@gmail.com>
2023-04-25 03:53:12 +0700

    nambahin codingan di file test dart nya

--- task_sheduler_rozi/lib/function/test.dart
+++ task_sheduler_rozi/lib/function/test.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class FileTest extends StatelessWidget {
+  const FileTest({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
