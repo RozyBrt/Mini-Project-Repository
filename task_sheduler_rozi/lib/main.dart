cb4356355cbd3478f6d6a501820fd843babe7b75
Fachrur Rozi Syah Putra Berutu <rozymc99@gmail.com>
2023-04-28 01:02:48 +0700

    -

--- task_sheduler_rozi/lib/main.dart
+++ task_sheduler_rozi/lib/main.dart
@@ -4,9 +4,11 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'screen/home_screen.dart';
 import 'screen/login_screen.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   _MyAppState createState() => _MyAppState();
 }
@@ -35,7 +37,7 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: isLoggedIn ? LoginScreen() : HomeScreen(),
+      home: isLoggedIn ? const LoginScreen() : const HomeScreen(),
     );
   }
 }
